78ad55076a7330daef547b3ce9001ca47fbf3fe8
Luc Miaz <luc@miaz.ch>
2025-12-05 22:34:40 +0100

    Refactor cas_to_pubchem to use single_synonym_to_pubchem and simplify logic; add cas_to_inchi function for CAS to InChI conversion.

--- pubchem/pubchem.py
+++ pubchem/pubchem.py
@@ -60,28 +60,28 @@ def inchikey_to_pubchem(inchikey:str):
 
 def cas_to_pubchem(cas:Union[str,list], substance:bool):
     """
-    wrapper for single_cas_to_pubchem
+    wrapper for single_synonym_to_pubchem
 
     :params cas: `<str>` or `<list>` of length 3 with cas number to convert or list of cas
     :params substance: `<bool>` True if substance (sid), False if compound (cid)
     """
     fcas = []
-    if isinstance(cas, list) and not(len(cas)==3 and len(str(cas[0]))<8\
-            and len(str(cas[1]))==2 and len(str(cas[2]))==1):
-        #there are multiple cas
-        fcas = [format_cas(c) for c in cas]
-    elif isinstance(cas,str):
+    # if isinstance(cas, list) and not(len(cas)==3 and len(str(cas[0]))<8\
+    #       and len(str(cas[1]))==2 and len(str(cas[2]))==1):
+    #     #there are multiple cas
+    #     fcas = [format_cas(c) for c in cas]
+    # elif 
+    if isinstance(cas,str):
         fcas = [cas]
-    elif isinstance(cas,list) and len(cas)==3 and len(str(cas[0]))<8\
-            and len(str(cas[1]))==2 and len(str(cas[2]))==1:
-        fcas =[format_cas(cas)]
     elif cas is None:
         return {}, []
+    else:
+        fcas = cas
     # Divide the process into 10 increments wait 1s between each
     return_dict = {}
     failed = []
-    for _cas in tqdm(fcas, total =len(fcas)):
-        x=single_cas_to_pubchem(_cas,substance=substance)
+    for i,_cas in tqdm(enumerate(fcas), total=len(fcas)):
+        x=single_synonym_to_pubchem(_cas,substance=substance)
         if x is None:
             failed.append(_cas)
         else:
@@ -100,9 +100,67 @@ def get_chunks(l:list, n:int):
     except TypeError as e:
         logging.info(e)
         return []
-    except KeyError as e:
-        logging.info(e)
-        return []
+
+def cas_to_inchi(cas:Union[str,list], max_query:int = 100):
+    """
+    Convert CAS number(s) to InChI string(s)
+
+    :params cas: `<str>` or `<list>` of CAS numbers to convert
+    :params max_query: chunk size for batch queries
+    :return: dictionary mapping CAS to SMILES, and list of failed CAS
+    """
+    # First try to get CIDs directly from CAS
+    cas_cids, failed = cas_to_pubchem(cas, substance=False)
+    
+    # the next part is not necessary anymore since CIDs are fetched directly from CAS even for substances
+    # # For failed CAS, try to get SIDs
+    # if failed:
+    #     cas_sids, still_failed = cas_to_pubchem(failed, substance=True)
+        
+    #     # Convert SIDs to CIDs
+    #     if cas_sids:
+    #         all_sids = [sid for sids in cas_sids.values() for sid in sids]
+    #         sids_to_cids_map = get_cids_from_sids(all_sids)
+            
+    #         # Map back to CAS
+    #         for cas_num, sids in cas_sids.items():
+    #             cids_from_sids = []
+    #             for sid in sids:
+    #                 cid = sids_to_cids_map.get(sid)
+    #                 if cid:
+    #                     if isinstance(cid, list):
+    #                         cids_from_sids.extend(cid)
+    #                     else:
+    #                         cids_from_sids.append(cid)
+    #             if cids_from_sids:
+    #                 cas_cids[cas_num] = cids_from_sids
+    #                 still_failed.remove(cas_num) if cas_num in still_failed else None
+    #     failed = still_failed
+    
+    # Get SMILES for all CIDs
+    cas_inchi = {}
+    all_cids = list(set([cid for cids in cas_cids.values() for cid in cids]))
+    
+    if all_cids:
+        cids_chunks = get_chunks(all_cids, max_query)
+        cid_to_inchi = {}
+        
+        for chunk in cids_chunks:
+            properties = get_from_cids(chunk, target='property/InChI')
+            if properties:
+                for prop in properties.get('PropertyTable', {}).get('Properties', []):
+                    cid = prop.get('CID')
+                    inchi = prop.get('InChI')
+                    if cid and inchi:
+                        cid_to_inchi[cid] = inchi
+
+        # Map InChI back to CAS
+        for cas_num, cids in cas_cids.items():
+            inchi_list = [cid_to_inchi.get(cid) for cid in cids if cid in cid_to_inchi]
+            if inchi_list:
+                cas_inchi[cas_num] = inchi_list[0] if len(inchi_list) == 1 else inchi_list
+
+    return cas_inchi, failed
 
 def cids_to_cas_and_einecs(cids:list, max_query:int = 100):
     """Legacy function, replaced by cids_to_cas_and_einecs_and_dtx"""
@@ -145,7 +203,6 @@ def single_synonym_to_pubchem(cas,substance:bool=False):
 
     :params cas: `<str>` or `<list>`
     :params substance: `<bool>` True if substance (sid), False if compound (cid)
-
     :return: cid or sid
     """
     if substance is True:
@@ -166,7 +223,7 @@ def single_synonym_to_pubchem(cas,substance:bool=False):
         return data.get('IdentifierList',{}).get('SID',[])
     else:
         return data.get('IdentifierList',{}).get('CID',[])
-    
+
 
 def SMILES_to_pubchem(smiles):
     """Use PubChem REST to convert CAS or other synonyms to CID 
@@ -176,7 +233,7 @@ def SMILES_to_pubchem(smiles):
 
     :return: cid or sid
     """
-    url = f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/smiles/{smiles}/cids/JSON"
+    url = f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/smiles/{smiles.replace('#','%23')}/cids/JSON"
     response = safe_request(url)
     try:
         data = json.loads(response)
@@ -446,7 +503,7 @@ def download_cids_by_hnid(hnid:int, folder_path:str, force:bool = False):
     with open(filename, 'r') as f:
             return json.load(f)
 
-def get_HSDB(cids:Union[list,int], chunk_size = 200):
+def get_HSDB(cids:Union[list,int]):
     """
     returns HSDB id from list of cids    
     """
@@ -461,20 +518,10 @@ def get_HSDB(cids:Union[list,int], chunk_size = 200):
         cids = [cids]
     elif not isinstance(cids,list):
         raise ValueError("cids should be an integer of a list of integers")
-    info =[]
-    for chunk in get_chunks(cids,chunk_size):
-        ret = get_from_cids(chunk,target='synonyms').get("InformationList",{}).get('Information',{})
-        if ret is not None:
-            try:
-                info+=ret
-            except Exception as e:
-                print(f"Exception for {chunk,}\n{e}")
+    info = get_from_cids(cids,target='synonyms').get("InformationList",{}).get('Information',{})
     for d in info:
         cid = d['CID']
-        try:
-            data[cid] = find_entry(d['Synonym'])
-        except KeyError:
-            data[cid] = None
+        data[cid] = find_entry(d['Synonym'])
     if len(cids)==1:
         return data[cids[0]]
     return data
